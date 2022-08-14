0b0ca5c6ce5d81f6156ac3fee2754d686d9b69c8
milena-mognon <milenamognon@gmail.com>
2022-08-14 16:07:46 -0300

    style: add sidebar style

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import { Header } from "./components/Header";
 import { Post } from "./Post";
 import "./global.css";
+import styles from "./App.module.css";
+import { Sidebar } from "./components/Sidebar";
 
 function App() {
   return (
     <>
       <Header />
-      <Post author="Milena" content="Teste" />
-      <Post author="Milena" content="Teste" />
-      <Post author="Milena" content="Teste" />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post author="Milena Mognon" content="Teste de conteudo" />
+          <Post author="Milena Mognon" content="Teste de conteudo" />
+          <Post author="Milena Mognon" content="Teste de conteudo" />
+        </main>
+      </div>
     </>
   );
 }
